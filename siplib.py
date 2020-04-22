53165c98a2c35888f2cad7e07ef00491c4a9c5a7
lglanville <lachlanglanville@gmail.com>
2020-04-22 10:36:27 +1000

    Major changes

--- siplib.py
+++ siplib.py
@@ -8,8 +8,8 @@ import argparse
 import zipfile
 import getpass
 import pathlib
-from fnmatch import fnmatch
 import logging
+from io import BytesIO
 FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('siplog')
@@ -19,17 +19,27 @@ logger.setLevel(logging.INFO)
 class Sip(zipfile.ZipFile):
     def __init__(self, fpath):
         if os.path.exists(fpath):
-            os.remove(fpath)
-            print("Haven't implemented this bit yet")
-        #init an empty sip
-        super(Sip, self).__init__(fpath, 'w')
-        self.sipref = str(uuid4())
-        self.xip = etree.Element(
-            'XIP',
-            nsmap={None: "http://preservica.com/XIP/v6.0"})
-        self.content = os.path.join(self.sipref, 'content')
-        self.filecount = 0
-        self.filesize = 0
+            logger.info(f'Opening existing SIP at {fpath}')
+            super(Sip, self).__init__(fpath, 'a')
+            for file in self.filelist:
+                if file.filename.endswith('.protocol'):
+                    prot = file.filename
+                    self.sipref = prot.split('.')[0]
+                if file.filename.endswith('metadata.xml'):
+                    self.xip = etree.parse(BytesIO(self.read(file.filename))).getroot()
+            self.protocol = etree.parse(BytesIO(self.read(prot)))
+            self.content = os.path.join(self.sipref, 'content')
+        else:
+            logger.info(f'Creating new SIP at {fpath}')
+            super(Sip, self).__init__(
+                fpath, 'w', compression=zipfile.ZIP_DEFLATED)
+            self.sipref = str(uuid4())
+            self.xip = etree.Element(
+                'XIP',
+                nsmap={None: "http://preservica.com/XIP/v6.0"})
+            self.content = os.path.join(self.sipref, 'content')
+            self.filecount = 0
+            self.filesize = 0
 
     def get_structs(self):
         structs = {}
@@ -41,11 +51,22 @@ class Sip(zipfile.ZipFile):
 
     def get_infobjs(self):
         infobjs = {}
-        for e in self.xip.findall('InformationObject', namespaces=self.xip.nsmap):
-            title = e.find('Title', namespaces=self.xip.nsmap).text
-            ref = e.find('Ref', namespaces=self.xip.nsmap).text
-            infobjs[title] = ref
+        for e in self.xip.findall('InformationObject'):
+            title = e.find('Title').text
+            ref = e.find('Ref').text
+            infobjs[ref] = title
         return infobjs
+
+    def get_checksums(self):
+        sums = {}
+        for elem in self.xip.findall('Bitstream'):
+            name = elem.find('Filename').text
+            sums[name] = {}
+            for fixity in elem.findall('Fixities/Fixity'):
+                alg = fixity.find('FixityAlgorithmRef').text
+                hash = fixity.find('FixityValue').text
+                sums[name][alg] = hash
+        return sums
 
     def add_structobj(self, title, parent_ref=None, security_tag='open'):
         """
@@ -60,7 +81,7 @@ class Sip(zipfile.ZipFile):
         etree.SubElement(sobj, 'Ref').text = ref
         etree.SubElement(sobj, 'Title').text = title
         etree.SubElement(sobj, 'SecurityTag').text = security_tag
-        if parent_ref:
+        if parent_ref is not None:
             etree.SubElement(sobj, 'Parent').text = parent_ref
         return ref
 
@@ -82,7 +103,7 @@ class Sip(zipfile.ZipFile):
         etree.SubElement(sobj, 'Parent').text = folder_ref
         return ref
 
-    def add_representation(self, name, info_ref, type, c_objects):
+    def add_representation(self, name, info_ref, c_objects, type='Preservation'):
         """
         A way of viewing the information within an asset. The most common
         example of different representations would be a preservation copy and
@@ -90,7 +111,12 @@ class Sip(zipfile.ZipFile):
         preservation purposes, and a downscaled web-ready MP4 for access
         purposes. Information objects will always have at least one
         representation (the preservation representation).
+        Representation names usually follow convention of type+ascending
+        number, ie Preservation-1, but can be descriptive.
         """
+        if type not in ('Access', 'Preservation'):
+            raise ValueError(
+                'Representation type must be Access or Preservation')
         rep = etree.SubElement(self.xip, 'Representation')
         logger.info(f'Adding Representation {name} to {info_ref}')
         etree.SubElement(rep, 'InformationObject').text = info_ref
@@ -103,7 +129,7 @@ class Sip(zipfile.ZipFile):
     def add_contobj(self, fname, info_ref, security_tag='open'):
         """
         A logically atomic piece of content, for example an attachment or an
-        email. Links to multiple bitstreams
+        email.
         """
         ref = str(uuid4())
         logger.info(f'Adding ContentObject {fname} {ref} to {info_ref}')
@@ -135,7 +161,7 @@ class Sip(zipfile.ZipFile):
         etree.SubElement(gen, 'Formats')
         etree.SubElement(gen, 'Properties')
 
-    def add_bitstream(self, fpath, checksums):
+    def add_bitstream(self, fpath, checksums, write=True):
         """
         The physical content, as stored on a storage adapter. In almost all
         cases, a generation of a CO will have only one bitstream; exceptions
@@ -144,19 +170,26 @@ class Sip(zipfile.ZipFile):
         Links to a content object.
         """
         fpath = pathlib.Path(fpath)
+        if fpath.is_absolute():
+            raise ValueError('Bitstream paths must be relative:', fpath)
         logger.info(f'Writing {fpath} to package')
-        self.write(fpath, self.content / fpath)
+        if write:
+            self.write(fpath, self.content / fpath)
         self.filecount += 1
         size = fpath.stat().st_size
         self.filesize += size
         bstream = etree.SubElement(self.xip, 'Bitstream')
         path, name = os.path.split(fpath)
         posix_path = fpath.parent.as_posix()
+        if posix_path == '.':
+            posix_path = ''
         etree.SubElement(bstream, 'Filename').text = name
         etree.SubElement(bstream, 'FileSize').text = str(size)
         etree.SubElement(bstream, 'PhysicalLocation').text = posix_path
         fixities = etree.SubElement(bstream, 'Fixities')
         for alg, hash in checksums.items():
+            if alg not in ['MD5', 'SHA1', 'SHA256', 'SHA512']:
+                raise ValueError('Unsupported algorithm:', alg)
             fixity = etree.SubElement(fixities, 'Fixity')
             etree.SubElement(fixity, 'FixityAlgorithmRef').text = alg
             etree.SubElement(fixity, 'FixityValue').text = hash
@@ -170,13 +203,13 @@ class Sip(zipfile.ZipFile):
         fpath = pathlib.Path(fpath)
         i = self.add_infobj(parent_ref, fpath.stem, security_tag=security_tag)
         c = self.add_contobj(fpath.name, i, security_tag=security_tag)
-        self.add_representation('Preservation-1', i, 'Preservation', [c])
+        self.add_representation('Preservation-1', i, [c])
         self.add_generation(c, '', [fpath])
         if checksum is None:
             checksum = sha256sum(fpath)
         self.add_bitstream(fpath, checksum)
 
-    def write_protocol(self, parent, name):
+    def write_protocol(self, parent_ref, name):
         prot = etree.Element(
             'protocol',
             nsmap={None: "http://www.tessella.com/xipcreateprotocol/v1"})
@@ -187,7 +220,7 @@ class Sip(zipfile.ZipFile):
         etree.SubElement(prot, 'submissionName').text = name
         etree.SubElement(prot, 'catalogueName').text = name
         etree.SubElement(prot, 'localAIP').text = self.sipref
-        etree.SubElement(prot, 'globalAIP').text = parent
+        etree.SubElement(prot, 'globalAIP').text = parent_ref
         etree.SubElement(prot, 'createdBy').text = getpass.getuser()
         tree = etree.ElementTree(prot)
         self.writestr(self.sipref+'.protocol', etree.tostring(
@@ -203,7 +236,16 @@ class Sip(zipfile.ZipFile):
                 tree, pretty_print=True, encoding="UTF-8",
                 xml_declaration=True, standalone=True))
 
-    def add_identifier(self, targetref, value, type='UMA'):
+    def serialise(self, parent_ref, name):
+        self.write_xip()
+        self.write_protocol(parent_ref, name)
+        self.close()
+
+    def add_identifier(self, targetref, value, type='code'):
+        """
+        Identifiers can be attached to StructuralObjects,
+        InformationObjects or ContentObjects.
+        """
         logger.info(f'Adding Identifier {type} {value} to {targetref}')
         ident = etree.SubElement(self.xip, 'Identifier')
         etree.SubElement(ident, 'Type').text = type
@@ -211,8 +253,11 @@ class Sip(zipfile.ZipFile):
         etree.SubElement(ident, 'Entity').text = targetref
 
     def add_metadata(self, targetref, fragment):
-        '''metadata fragment can be attached to
-        StructuralObjects or InformationObjects'''
+        """
+        Metadata fragments can be attached to StructuralObjects,
+        InformationObjects or ContentObjects. fragment is the root element of
+        an xml tree.
+        """
         ref = str(uuid4())
         nspace = fragment.tag.split('}')[0].strip('{')
         logger.info(f'Adding Metadata {nspace} to {targetref}')
@@ -222,6 +267,21 @@ class Sip(zipfile.ZipFile):
         content = etree.SubElement(metadata, 'Content')
         content.append(fragment)
         return ref
+
+    def add_extendedxip(self, targetref, earliest, latest, surrogate=True):
+        nspace = "http://preservica.com/ExtendedXIP/v6.0"
+        ref = str(uuid4())
+        metadata = etree.SubElement(self.xip, 'Metadata', schemaUri=nspace)
+        etree.SubElement(metadata, 'Ref').text = ref
+        etree.SubElement(metadata, 'Entity').text = targetref
+        content = etree.SubElement(metadata, 'Content')
+        ex_xip = etree.SubElement(content, 'ExtendedXIP', nsmap={None: nspace})
+        if surrogate:
+            etree.SubElement(ex_xip, 'DigitalSurrogate').text = 'true'
+        else:
+            etree.SubElement(ex_xip, 'DigitalSurrogate').text = 'false'
+        etree.SubElement(ex_xip, 'CoverageFrom').text = earliest
+        etree.SubElement(ex_xip, 'CoverageTo').text = latest
 
 
 def sha256sum(filename):
@@ -247,92 +307,28 @@ def suffsplit(fname, separator):
     return (pref, suff, ext)
 
 
-def get_hash(hashfile):
-    with open(hashfile) as f:
-        alg = hashfile.split('.')[-1]
-        hash = f.read()
-    return {alg: hash}
-
-
-def build_filemap(basedir, multi=False, sep=None):
-    obj = {'files': 0, 'sizes': 0, 'metadata': [], 'filemap': {}}
-    hashfiles = []
-    for root, _, files in os.walk(sys.argv[1]):
-        for file in files:
-            relpath = os.path.relpath(os.path.join(root, file))
-            if file.endswith(('sha256', 'sha512', 'sha1', 'md5')):
-                hashfiles.append(relpath)
-            elif file.endswith('.metadata'):
-                obj['metadata'].append(relpath)
-            else:
-                obj['files'] += 1
-                obj['sizes'] += os.path.getsize(relpath)
-                if sep is not None:
-                    pref, suff, ext = suffsplit(file, sep)
-                else:
-                    pref, ext = os.path.splitext(file)
-                label = 'Preservation'
-                if multi:
-                    if ext.lower() != ('.wav', '.tif', '.mxf'):
-                        label = 'Access'
-                entry = {'path': relpath, 'label': label, 'hashes': {}}
-                if pref in obj['filemap'].keys():
-                    obj['filemap'][pref].append(entry)
-                else:
-                    obj['filemap'][pref] = [entry]
-    for file in hashfiles:
-        alg = file.split('.')[-1].upper()
-        with open(file, newline='') as f:
-            for line in f.readlines():
-                line = line.strip().split(maxsplit=1)
-                if len(line) == 2:
-                    print(line)
-                    hash, file = line
-                    for pref, bstreams in obj['filemap'].items():
-                        for bstream in bstreams:
-                            print(bstream)
-                            if fnmatch(bstream['path'], file):
-                                entry['hashes'][alg] = hash
-    return obj
-
-
-def main(basedir, outdir, parent=None, sep=None, multi=False):
-    sipname = os.path.split(basedir)[1]
-    sip = Sip(os.path.join(outdir, sipname+'.zip'))
+def main(basedir, outdir, parent=None, security='open'):
     os.chdir(basedir)
-    obj = build_filemap(sys.argv[1], sep=sep, multi=multi)
-    base = sip.add_structobj(sipname)
-    for asset, entries in obj['filemap'].items():
-        inf_obj = sip.add_infobj(asset, base)
-        for entry in entries:
-            con_obj = sip.add_contobj(os.path.split(entry['path'])[1], inf_obj)
-            sip.add_representation(os.path.split(entry['path'])[0], inf_obj, entry['label'], [con_obj])
-            sip.add_generation(con_obj, os.path.split(entry['path'])[0], [entry['path']])
-            if entry['hashes'] == {}:
-                entry['hashes'].update(sha256sum(entry['path']))
-            sip.add_bitstream(entry['path'], entry['hashes'])
-    for meta in obj['metadata']:
-        e = etree.parse(meta)
-        fragment = e.getroot()
-        sip.add_metadata(base, fragment)
-    sip.write_protocol(parent, sipname)
+    sip = Sip(os.path.join(outdir, os.path.split(basedir)[1]+'.zip'))
+    for root, dirs, files in os.walk(os.getcwd()):
+        parent = sip.add_structobj(
+            os.path.split(root)[1], parent_ref=parent, security_tag=security)
+        for file in files:
+            fpath = pathlib.Path(root) / file
+            relpath = fpath.relative_to(fpath.cwd())
+            sip.add_tree(parent, relpath, security_tag=security)
     sip.write_xip()
+    sip.write_protocol(parent, os.path.split(sys.argv[1])[1])
     sip.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Build a SIP from a directory')
+        description='Build a simple SIP from a directory')
     parser.add_argument(
         'dir', metavar='i', type=str, help='base directory for a SIP')
     parser.add_argument(
         'out', metavar='o', type=str, help='base directory for a SIP')
-    parser.add_argument(
-        '--separator', type=str,
-        help='filename separator to split a filename on')
-    parser.add_argument(
-        '--identifier', type=str,
-        help='filename separator to split a filename on')
     parser.add_argument(
         '--target', type=str,
         help='target folder for sip')
@@ -341,4 +337,4 @@ if __name__ == '__main__':
         help='security tag')
 
     args = parser.parse_args()
-    main(args.dir, args.out, parent=args.target, sep=args.separator)
+    main(args.dir, args.out, parent=args.target)
