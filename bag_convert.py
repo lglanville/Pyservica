cb9f63afa2f856e2fa83456212551990d4be15c4
lglanville <lachlanglanville@gmail.com>
2020-05-11 10:51:22 +1000

    Removed a couple of files that snuck in, updated bag_convert (untested).

--- bag_convert.py
+++ bag_convert.py
@@ -4,25 +4,31 @@ import os
 import sys
 import pathlib
 
-bag = bagit.Bag(sys.argv[1])
-os.chdir(bag.path)
-sip = siplib.Sip(os.path.join(sys.argv[2], os.path.split(sys.argv[1])[1]))
-parent = "e9c10eef-9365-4b11-be58-2975cb1cc20b"
-for root, dirs, files in os.walk('data/objects'):
-    if root == 'data/objects':
-        cdir = sip.add_struct(bag.info['identifier'], parent_ref=parent)
-        sip.add_identifier(cdir, bag.info['identifier'])
-    else:
-        cdir = sip.add_struct(os.path.split(root)[1], parent_ref=cdir)
-    for file in files:
-        fpath = pathlib.Path(root) / file
-        hash = [hash for file, hash in bag.payload_entries().items() if pathlib.Path(file) == fpath]
-        if len(hash) == 1:
-            norm_hash = {alg.upper(): val for alg, val in hash[0].items()}
-            sip.add_tree(parent, fpath, checksum=norm_hash)
-        else:
-            raise ValueError('Too many hashes')
 
-sip.write_xip()
-sip.write_protocol(parent, os.path.split(sys.argv[1])[1])
-sip.close()
+def main(bagdir, outdir, parent_ref):
+    bag = bagit.Bag(bagdir)
+    os.chdir(bag.path)
+    bag_path = pathlib.Path(bagdir)
+    sip_path = pathlib.Path(outdir, bag_path.name+'.zip')
+    sip = siplib.Sip(sip_path, parent_ref)
+    for root, dirs, files in os.walk('data/objects'):
+        if root == 'data/objects':
+            parent_ref = sip.add_structobj(
+                bag.info['identifier'], parent_ref=parent_ref)
+            sip.add_identifier(parent_ref, bag.info['identifier'])
+        else:
+            parent_ref = sip.add_structobj(
+                os.path.split(root)[1], parent_ref=parent_ref)
+        for file in files:
+            fpath = pathlib.Path(root) / file
+            hash = [hash for file, hash in bag.payload_entries().items() if pathlib.Path(file) == fpath]
+            if len(hash) == 1:
+                norm_hash = {alg.upper(): val for alg, val in hash[0].items()}
+                sip.add_tree(parent_ref, fpath, checksum=norm_hash)
+            else:
+                raise ValueError('Too many hashes')
+    sip.serialise()
+    sip.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
